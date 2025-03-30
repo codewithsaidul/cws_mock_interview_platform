24cd28afcea0b6292fca7f2e086849fa5e905a5f
codewithsaidul <codewithsaidul@gmail.com>
2025-03-30 17:46:06 +0600

    🏠 Home Page Ui Completed with dummy data

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,60 @@
+import InterviewCard from "@/components/InterviewCard";
+import { Button } from "@/components/ui/button";
+import { dummyInterviews } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <>
+      {/* ==================== Header of Home Page ==================== */}
+      <section className="card-cta">
+        <div className="flex flex-col gap-6 max-w-lg">
+          <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
+          <p className="text-lg">
+            Practice real interview questions & get instant feedback
+          </p>
+
+          <Button asChild className="btn-primary max-sm:w-full">
+            <Link href="/interview">Start an Interview</Link>
+          </Button>
+        </div>
+
+        <Image
+          src="/robot.png"
+          alt="robo-dude"
+          width={400}
+          height={400}
+          className="max-sm:hidden"
+        />
+      </section>
+
+      {/* Section displaying the "Your Interviews" heading. Shows a placeholder message when no interviews are taken yet, and will later display dynamic interview data when available. */}
+
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Your Interviews</h2>
+        <div className="interviews-section">
+          {dummyInterviews.map((interview) => (
+            <InterviewCard key={interview.id} {...interview} />
+          ))}
+          {/* <p>You Haven&apos;t taken any interviews yet</p> */}
+        </div>
+      </section>
+
+      {/* Section displaying the "Take Interviews" heading and a placeholder message when no interviews are available, and will later display dynamic interview data when available. */}
+
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Take Interviews</h2>
+
+        <div className="interviews-section">
+          {dummyInterviews.map((interview) => (
+            <InterviewCard key={interview.id} {...interview} />
+          ))}
+          {/* <p>There are no interviews available</p> */}
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default HomePage;
